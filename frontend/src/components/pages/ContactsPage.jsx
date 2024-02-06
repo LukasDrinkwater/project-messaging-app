8fe47e9c761a80c8276c1330f153172d546d849f
Lukas <lukasdrinkwater97@gmail.com>
2024-02-06 12:22:51 +0000

    initial commit

---
+++ frontend/src/components/pages/ContactsPage.jsx
@@ -0,0 +1,11 @@
+function ContactsPage() {
+  return (
+    <>
+      <div className="contactPageContainer">
+        <div className="allContacts">
+          {/* map through contacts and make a preview */}
+        </div>
+      </div>
+    </>
+  );
+}
