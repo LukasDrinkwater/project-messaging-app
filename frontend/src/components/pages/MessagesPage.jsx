5a50e56f3ca86977aaf02b33ec0edf6592633beb
Lukas <lukasdrinkwater97@gmail.com>
2024-02-06 12:23:07 +0000

    initial commit

---
+++ frontend/src/components/pages/MessagesPage.jsx
@@ -0,0 +1,12 @@
+export default function Messages() {
+  return (
+    <>
+      <div className="messagePageContainer">
+        <div className="messageFeed">
+          {/* map and display messages */}
+          <p>messages</p>
+        </div>
+      </div>
+    </>
+  );
+}
