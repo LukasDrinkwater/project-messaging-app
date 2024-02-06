17cdc1f3b659a9ee4eac144855b73fafb33ddfd4
Lukas <lukasdrinkwater97@gmail.com>
2024-02-06 12:24:43 +0000

    initial commit and started boiler plate code

---
+++ frontend/src/components/single-components/contactPreview.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function contactPreview() {
+  return (
+    <>
+      <div className="contactPreview">
+        <div className="profilePicture">
+          <img src="" alt="" />
+        </div>
+        <div className="contactPreviewName">
+          <p>{/* contacts name */}</p>
+        </div>
+        <div className="editContact">
+          <button>EDIT CONTACT</button>
+        </div>
+      </div>
+    </>
+  );
+}
