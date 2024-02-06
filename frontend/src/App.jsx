530a78dc1c0688796d83db55ea329da6650489b2
Lukas <lukasdrinkwater97@gmail.com>
2024-02-06 12:25:01 +0000

    added redirect from base url /

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, createContext, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import "./App.css";
 
@@ -7,7 +7,10 @@ import "./App.css";
 export const LoginContext = createContext();
 
 // Import component pages
+import Footer from "./components/single-components/single_use/Footer";
+import Header from "./components/single-components/single_use/Header";
 import LoginPage from "./components/pages/LoginPage";
+import MessagesPage from "./components/pages/MessagesPage";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState({ loggedIn: false, admin: false });
@@ -15,42 +18,48 @@ function App() {
   // runs on first page load. Initial load or after a user leaves and comes back to the
   // site and the session cookie still exists. Then updates loggedIn state
 
-  useEffect(() => {
-    const checkIfLoggedIn = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/check-auth", {
-          credentials: "include",
-          mode: "cors",
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+  // useEffect(() => {
+  //   const checkIfLoggedIn = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         "http://localhost:3000/authentication/check-auth",
+  //         {
+  //           credentials: "include",
+  //           mode: "cors",
+  //           method: "GET",
+  //           headers: {
+  //             "Content-Type": "application/json",
+  //           },
+  //         }
+  //       );
 
-        if (response.ok) {
-          const data = await response.json();
-          if (data.loggedIn) {
-            setLoggedIn(true);
-          } else {
-            // Redirect to login page
-          }
-        }
-      } catch (error) {
-        console.log("Error checking logged in status:", error);
-      }
-    };
-    checkIfLoggedIn();
-  });
+  //       if (response.ok) {
+  //         const data = await response.json();
+  //         if (data.loggedIn) {
+  //           setLoggedIn(true);
+  //         } else {
+  //           // Redirect to login page
+  //         }
+  //       }
+  //     } catch (error) {
+  //       console.log("Error checking logged in status:", error);
+  //     }
+  //   };
+  //   checkIfLoggedIn();
+  // });
 
   return (
     <>
       <LoginContext.Provider value={[loggedIn, setLoggedIn]}>
         <div className="content">
-          {/* <Header /> */}
+          <Header />
           <Routes>
+            <Route path="/" Component={MessagesPage}></Route>
+
             <Route path="/login" Component={LoginPage}></Route>
+            <Route path="/messages" Component={MessagesPage}></Route>
           </Routes>
-          <div>Hey look im working</div>
+          <Footer />
         </div>
       </LoginContext.Provider>
     </>
