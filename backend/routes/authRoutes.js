f7afa83848bbb137585bc19bf9d4d389c831808c
Lukas <lukasdrinkwater97@gmail.com>
2024-02-08 20:52:12 +0000

    added test route to test api

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+// Passxport for login
+const passport = require("passport");
 const authController = require("../controllers/authController");
+const { login } = require("../strategies/authentication");
 
 // GET test reponse
 router.get("/check-auth", authController.test_check);
+
+// POST Login form
+// when passport.authenticate is called its going through the localStrategy defined
+// in app.js
+router.post("/login", passport.authenticate("local"), login);
+
+// POST signup new user
+router.post("/signup", authController.signup_post);
+
+// POST logout
+router.post("/logout", authController.logout_post);
 
 module.exports = router;
