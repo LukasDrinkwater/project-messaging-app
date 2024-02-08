85399e67d8ae386267bbc2d83197091b8c5b6b48
Lukas <lukasdrinkwater97@gmail.com>
2024-02-08 20:50:45 +0000

    added signup post and logout post handlers

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,64 @@
 const { body, validationResult } = require("express-validator");
 const asnycHandler = require("express-async-handler");
 
+const User = require("../models/Users");
+
+const passport = require("passport");
+
 // Test API GET
 exports.test_check = asnycHandler(async (req, res, next) => {
   console.log(res);
   res.json({ message: "hey im a test reponse" }).statusCode(200).send();
 });
+
+// POST signup new user
+exports.signup_post = [
+  body("username", "Username must be atleast 1 character")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("password", "Password must be atleast 1 character long")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("email", "email must be valid").isEmail().escape(),
+
+  asnycHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    // const errors = [];
+
+    if (!errors.isEmpty()) {
+      res.sendStatus(400);
+      return;
+    } else {
+      // create a new user
+      const newUser = new User({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+      });
+      console.log("saving user");
+      await newUser.save();
+      console.log("user saved");
+      res.statusCode(200);
+    }
+  }),
+];
+
+exports.logout_post = asnycHandler(async (req, res, next) => {
+  req.logout((error) => {
+    // res.session.destroy() gets rid of the cookie
+    req.session.destroy();
+    if (error) {
+      return next(error);
+    }
+    res.status(200).clearCookie("connect.sid").send();
+  });
+});
+
+// username:
+// password:
+// email: {
+// messages:
+// group: [{
+// contacts:
