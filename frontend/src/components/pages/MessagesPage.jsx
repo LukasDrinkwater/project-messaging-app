edefd0aaf92fc691b9e51a240dd609f156b6cf7f
Lukas <lukasdrinkwater97@gmail.com>
2024-02-08 20:55:01 +0000

    started working on message axios request

--- frontend/src/components/pages/MessagesPage.jsx
+++ frontend/src/components/pages/MessagesPage.jsx
@@ -1,7 +1,32 @@
+import axios from "axios";
+import "./MessagesPage.css";
+import { useEffect, useState } from "react";
+
 export default function Messages() {
+  const [userReceiver, setUserReceiver] = useState(undefined);
+
+  useEffect(() => {
+    const getCurrentChats = async () => {
+      try {
+        const response = await axios({
+          method: "GET",
+          url: "/messages/allChats",
+          credentials: "true",
+        });
+
+        if (response.ok) {
+        }
+      } catch (error) {
+        console.log("Error getting all chats:", error);
+      }
+    };
+    getCurrentChats();
+  }, []);
+
   return (
     <>
       <div className="messagePageContainer">
+        <div className="currentChats"></div>
         <div className="messageFeed">
           {/* map and display messages */}
           <p>messages</p>
