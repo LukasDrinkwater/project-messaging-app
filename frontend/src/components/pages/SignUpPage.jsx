042a0ff236a04f970e168c9207c5c5a8f7311a8b
Lukas <lukasdrinkwater97@gmail.com>
2024-02-08 20:55:25 +0000

    finished signup page component and test it to see if it was working

---
+++ frontend/src/components/pages/SignUpPage.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+// Import components
+import SignUpForm from "../single-components/forms/SignUpForm";
+
+export default function SignUpPage() {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  );
+}
