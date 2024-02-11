3db33f60f856389fb25f54984e9806f2ae373a99
Lukas <lukasdrinkwater97@gmail.com>
2024-02-11 21:40:41 +0000

    got fetch request working. Added contactPreview component call and map

--- frontend/src/components/pages/ContactsPage.jsx
+++ frontend/src/components/pages/ContactsPage.jsx
@@ -1,11 +1,81 @@
-function ContactsPage() {
+import { useEffect, useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import "./ContactPage.css";
+
+import axios, { all } from "axios";
+
+// Import components
+import AddContactForm from "../single-components/forms/AddContactForm";
+import ContactPreview from "../single-components/contacts/ContactPreview";
+
+export default function ContactsPage() {
+  const [addContact, setAddContact] = useState(false);
+  const [allContacts, setAllContacts] = useState([]);
+
+  useEffect(() => {
+    console.log("trigger");
+    const getAllContacts = async () => {
+      try {
+        const response = await axios({
+          method: "GET",
+          url: "/api/contacts/users-contacts",
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        console.log("triggered");
+
+        console.log(response);
+        setAllContacts(response.data);
+      } catch (error) {
+        console.log("Error getting contacts:", error);
+      }
+    };
+    // only run the api request is the user isnt on the add contact form
+    if (!addContact) {
+      getAllContacts();
+    }
+  }, []);
+
+  // console.log(allContacts.length);
+
   return (
     <>
       <div className="contactPageContainer">
         <div className="allContacts">
-          {/* map through contacts and make a preview */}
+          {addContact ? (
+            <AddContactForm />
+          ) : (
+            /* map through contacts and make a preview */
+
+            <>
+              {allContacts.length > 0 &&
+                allContacts.map((contact) => (
+                  <ContactPreview key={contact.id} contact={contact} />
+                ))}
+            </>
+          )}
         </div>
+        {!addContact && (
+          <div className="addContactButton">
+            {/* <Link to="add">ADD NEW CONTACT</Link> */}
+            <button
+              onClick={() => {
+                setAddContact((prevState) => !prevState);
+              }}
+            >
+              GO TO ADD CONTACTS
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
 }
+// {
+//   allContacts &&
+//     allContacts.map((contact) => (
+//       <ContactPreview key={contact.id} contact={contact} />
+//     ));
+// }
