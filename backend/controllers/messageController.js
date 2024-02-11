87a38f0223a9b4e83e01fea5a0ac6c8fee5bdf60
Lukas <lukasdrinkwater97@gmail.com>
2024-02-11 21:36:09 +0000

    initial commit,m added handler for message preview and message post

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -6,7 +6,7 @@ const Users = require("../models/Users");
 const Messages = require("../models/Messages");
 const Groups = require("../models/Groups");
 
-exports.get_user_messages = asnycHandler(async (req, res, next) => {
+exports.get_user_message_preview = asnycHandler(async (req, res, next) => {
   const userSender = req.params.userSender;
   const userReceiver = req.params.userReceiver;
 });
