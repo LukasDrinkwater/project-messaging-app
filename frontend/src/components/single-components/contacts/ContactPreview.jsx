4baa251149467ba9b3abaf6c756a2a15bb6f93a1
Lukas <lukasdrinkwater97@gmail.com>
2024-02-11 21:41:08 +0000

    initial commit and added structure for component information

---
+++ frontend/src/components/single-components/contacts/ContactPreview.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function ContactPreview({ contact }) {
+  return (
+    <>
+      <div className="contactPreview">
+        <div className="profilePicture">
+          <img src="" alt="" />
+        </div>
+        <div className="contactPreviewName">
+          <p>{contact.username}</p>
+        </div>
+        <div className="contactPreviewMessage">
+          <p>{contact.messages.content}</p>
+        </div>
+        <div className="editContact">
+          <button>EDIT CONTACT</button>
+        </div>
+      </div>
+    </>
+  );
+}
