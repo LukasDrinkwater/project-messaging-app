c23809ac2682121a4741710d1c5424970f2c5719
Lukas <lukasdrinkwater97@gmail.com>
2024-02-11 21:38:26 +0000

    initial commit and added check login route and handler

--- backend/strategies/authentication.js
+++ backend/strategies/authentication.js
@@ -5,7 +5,7 @@ const passport = require("passport");
 const login = (req, res) => {
   // If this function is called, authentication was successful.
   // `req.user` contains the authenticated user.
-  console.log("login req.user", req.user);
+  // console.log("login req.user", req.user);
   // res.cookie("blog_cookie", "12345");
   res.status(200).json({ message: "Login successful", user: req.user });
 };
