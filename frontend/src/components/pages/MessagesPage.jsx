fb1479eb1b661ce5218bc87b2f0244a37a863727
Lukas <lukasdrinkwater97@gmail.com>
2024-02-12 20:42:32 +0000

    initial commit

--- frontend/src/components/pages/MessagesPage.jsx
+++ frontend/src/components/pages/MessagesPage.jsx
@@ -1,32 +1,52 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import "./MessagesPage.css";
 import { useEffect, useState } from "react";
 
 export default function Messages() {
+  const navigate = useNavigate();
   const [userReceiver, setUserReceiver] = useState(undefined);
+  const [currentChats, setCurrnentChats] = useState([]);
 
-  useEffect(() => {
-    const getCurrentChats = async () => {
-      try {
-        const response = await axios({
-          method: "GET",
-          url: "/messages/allChats",
-          credentials: "true",
-        });
+  // useEffect(() => {
+  //   const getCurrentChats = async () => {
+  //     try {
+  //       const response = await axios({
+  //         method: "GET",
+  //         url: "/messages/allChats",
+  //         credentials: "true",
+  //       });
 
-        if (response.ok) {
-        }
-      } catch (error) {
-        console.log("Error getting all chats:", error);
-      }
-    };
-    getCurrentChats();
-  }, []);
+  //       if (response.ok) {
+  //       }
+  //     } catch (error) {
+  //       console.log("Error getting all chats:", error);
+  //     }
+  //   };
+  //   getCurrentChats();
+  // }, []);
+
+  const handleNewChatClick = () => {
+    navigate("/contacts");
+  };
 
   return (
     <>
       <div className="messagePageContainer">
-        <div className="currentChats"></div>
+        <div className="currentChats">
+          {currentChats.length > 0 ? (
+            // map through chats to show a preview
+            <p>chat previews</p>
+          ) : (
+            // render new chat button
+            <>
+              <p>new chat</p>
+              <div className="newChatButton">
+                <button onClick={handleNewChatClick}>Start new chat</button>
+              </div>
+            </>
+          )}
+        </div>
         <div className="messageFeed">
           {/* map and display messages */}
           <p>messages</p>
