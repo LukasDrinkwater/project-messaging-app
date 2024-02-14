d81d4ac5ac59cd687dd1b85a878576a993ce8c65
Lukas <lukasdrinkwater97@gmail.com>
2024-02-14 22:20:38 +0000

    commit

--- backend/models/Groups.js
+++ backend/models/Groups.js
@@ -6,7 +6,7 @@ const GroupSchema = new Schema(
   {
     name: { type: String, required: true },
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    // messages: [{ type: mongoose.Schema.Types.ObjectId, red: "Message" }],
+    messages: [{ type: mongoose.Schema.Types.ObjectId, red: "Message" }],
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
