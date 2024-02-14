89e626a94fd8a1223571b56bd04da1cc4de79759
Lukas <lukasdrinkwater97@gmail.com>
2024-02-14 22:20:59 +0000

    commit

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -8,7 +8,7 @@ const UserSchema = Schema(
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    // messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
+    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
     chats: [{ type: mongoose.Schema.Types.ObjectId, ref: "Chat" }],
     contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" }],
