24a87bff1ae0ef0a97e7f7ef3ce2ce5802238106
Lukas <lukasdrinkwater97@gmail.com>
2024-02-14 22:16:56 +0000

    added usersChats state and set it in the axios response

--- frontend/src/components/pages/ContactsPage.jsx
+++ frontend/src/components/pages/ContactsPage.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import { NavLink, Link } from "react-router-dom";
+// import { NavLink, Link } from "react-router-dom";
 import "./ContactPage.css";
 
-import axios, { all } from "axios";
+import axios from "axios";
 
 // Import components
 import AddContactForm from "../single-components/forms/AddContactForm";
@@ -11,9 +11,11 @@ import ContactPreview from "../single-components/contacts/ContactPreview";
 export default function ContactsPage() {
   const [addContact, setAddContact] = useState(false);
   const [allContacts, setAllContacts] = useState([]);
+  const [usersChats, setUsersChats] = useState([]);
 
   useEffect(() => {
     console.log("trigger");
+
     const getAllContacts = async () => {
       try {
         const response = await axios({
@@ -26,8 +28,9 @@ export default function ContactsPage() {
         });
         console.log("triggered");
 
-        console.log(response);
-        setAllContacts(response.data);
+        // console.log(response.data);
+        setAllContacts(response.data.allContacts);
+        setUsersChats(response.data.usersChats);
       } catch (error) {
         console.log("Error getting contacts:", error);
       }
@@ -52,7 +55,11 @@ export default function ContactsPage() {
             <>
               {allContacts.length > 0 &&
                 allContacts.map((contact) => (
-                  <ContactPreview key={contact.id} contact={contact} />
+                  <ContactPreview
+                    key={contact.id}
+                    contact={contact}
+                    usersChats={usersChats}
+                  />
                 ))}
             </>
           )}
