09e68351af3101f381019027e0ba57d1cf6e412e
Lukas <lukasdrinkwater97@gmail.com>
2024-02-14 22:17:55 +0000

    added usersChatsProp and check to see if the current contact has a current chat

--- frontend/src/components/single-components/contacts/ContactPreview.jsx
+++ frontend/src/components/single-components/contacts/ContactPreview.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-export default function ContactPreview({ contact }) {
+export default function ContactPreview({ contact, usersChats }) {
+  const navigate = useNavigate();
   console.log(contact);
+
+  const startNewChat = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios({
+        method: "POST",
+        url: "/api/chats/new-chat",
+        withCredentials: true,
+        data: {
+          userToChatWith: contact.id,
+        },
+      });
+      const data = response.data;
+
+      if (data.chatCreated) {
+        navigate(`/messages/${data.chatId}`);
+      }
+    } catch (error) {
+      console.log("Error starting new chat:", error);
+    }
+  };
+
+  console.log(usersChats);
+  const contactId = contact.id;
+  const currentContactChat = usersChats.some((chat) =>
+    chat.users.some((user) => user._id === contactId)
+  );
+
+  // usersChats.find((chat) =>
+  //   chat.users.some((user) => user.id === contactId)
+  // );
+  console.log(contact.username, currentContactChat);
+  // const chatExists =
+
   return (
     <>
       <div className="contactPreview">
@@ -14,10 +51,12 @@ export default function ContactPreview({ contact }) {
         <div className="contactPreviewMessage">
           <p>{contact.messages.content}</p>
         </div>
-        <div className="editContact">
-          <button></button>
-        </div>
-        <div className="editContact">
+        {
+          <div className="startChatButton">
+            <button onClick={startNewChat}>Start Chat</button>
+          </div>
+        }
+        <div className="editContactButton">
           <button>EDIT CONTACT</button>
         </div>
       </div>
