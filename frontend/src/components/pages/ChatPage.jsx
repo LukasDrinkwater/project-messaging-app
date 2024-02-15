179bbbe0ad4c0c3293cd63fa16ac8bd8566fb781
Lukas <lukasdrinkwater97@gmail.com>
2024-02-15 21:52:45 +0000

    commit

--- frontend/src/components/pages/ChatPage.jsx
+++ frontend/src/components/pages/ChatPage.jsx
@@ -1,20 +1,30 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+// Import components
+import Chat from "../single-components/chats/Chat";
 
 export default function ChatPage() {
   const { chatId } = useParams();
+  const [chat, setChat] = useState([]);
+  const [userId, setUserId] = useState(null);
   // useEffect to get the chat from the url :chatId which comes from starting a new chat
   useEffect(() => {
     const getChat = async () => {
       try {
-        const response = axios({
+        const response = await axios({
           method: "GET",
           url: `/api/chats/${chatId}`,
           withCredentials: true,
         });
 
+        // console.log(response);
         // if reponse good assign data to state
+        if (response.data.chat) {
+          setChat(response.data.chat);
+          setUserId(response.data.userId);
+        }
 
         // if bad show error
       } catch (error) {
@@ -28,6 +38,8 @@ export default function ChatPage() {
   return (
     <>
       {/* render component for chat. */}
+      {!chat ? <div>Loading Chat</div> : <Chat chat={chat} userId={userId} />}
+
       <div>ChatPage</div>
     </>
   );
