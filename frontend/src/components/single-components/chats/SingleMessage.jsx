64b3d4580d463e78cba6469cada6a799c7b79da6
Lukas <lukasdrinkwater97@gmail.com>
2024-02-15 21:54:22 +0000

    initial commit and basic structure and adding props from Chat.jsx

---
+++ frontend/src/components/single-components/chats/SingleMessage.jsx
@@ -0,0 +1,18 @@
+export default function SingleMessage({ message, userId }) {
+  // set the message class if the message is from the user logged in or not
+  let messageClass = "";
+  if (message.sender.id === userId) {
+    messageClass = "user";
+  } else {
+    messageClass = "receiver";
+  }
+
+  return (
+    <>
+      <div className={messageClass}>
+        <p>{message.content}</p>
+        <p>{message.createdAtFormatted}</p>
+      </div>
+    </>
+  );
+}
