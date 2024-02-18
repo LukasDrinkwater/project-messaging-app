abba41ded9972e6fd56b24e737da6ecfabf21fd1
Lukas <lukasdrinkwater97@gmail.com>
2024-02-18 17:49:58 +0000

    commented out messages

--- backend/models/Chats.js
+++ backend/models/Chats.js
@@ -5,8 +5,10 @@ const Schema = mongoose.Schema;
 const ChatSchema = Schema(
   {
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
+    // messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
+    lastMessage: { type: String, required: false },
   },
+
   { timestamps: true, toJSON: { virtuals: true } }
 );
 
@@ -14,16 +16,27 @@ ChatSchema.virtual("lastUpdate").get(function () {
   return this.updatedAt.toLocalDateString;
 });
 
-ChatSchema.virtual("lastMessage").get(function () {
-  // if there are messages sort them and send back the latest message
-  if (this.messages.length > 0) {
-    const sortedMessages = this.messages.sort(
-      (a, b) => b.createdAt - a.createdAt
-    );
-    return sortedMessages[0];
-  } else {
-    return null;
+ChatSchema.virtual("lastMessageFormatted").get(function () {
+  if (this.lastMessage.length > 30) {
+    return this.lastMessage.slice(0, 30) + "...";
   }
+  return this.lastMessage;
 });
+
+// ChatSchema.virtual("lastMessage").get(async function () {
+//   // Populate the 'messages' field before sorting
+//   await this.populate("messages");
+//   // console.log("Populated messages:", this.messages);
+
+//   // If there are messages, sort them and send back the latest message
+//   if (this.messages.length > 0) {
+//     const sortedMessages = this.messages.sort(
+//       (a, b) => b.createdAt - a.createdAt
+//     );
+//     return sortedMessages[0];
+//   } else {
+//     return null;
+//   }
+// });
 
 module.exports = mongoose.model("Chat", ChatSchema);
