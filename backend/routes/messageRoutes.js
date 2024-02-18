d464ca374b5603cff58b86dba547ffcfc9d9ea65
Lukas <lukasdrinkwater97@gmail.com>
2024-02-18 17:50:15 +0000

    added new message post route

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const authController = require("../controllers/authController");
+const messageContoller = require("../controllers/messageController");
 
 // GET test reponse
-router.get("/check-auth", authController.test_check);
+// router.get("/check-auth", authController.test_check);
+
+// POST new message from chat
+router.post("/new-message", messageContoller.new_message_post);
 
 module.exports = router;
