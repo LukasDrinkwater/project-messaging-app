878e4fdd1c43403647e63a0ad253c983aaa84cd4
Lukas <lukasdrinkwater97@gmail.com>
2024-02-18 17:49:32 +0000

    commented out groups chats and messages object links

--- backend/models/Messages.js
+++ backend/models/Messages.js
@@ -6,11 +6,12 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema(
   {
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    receiver: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: false,
-    },
+    // receiver: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "User",
+    //   required: false,
+    // },
+    chat: [{ type: mongoose.Schema.Types.ObjectId, ref: "Chat" }],
     group: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Group",
@@ -22,7 +23,7 @@ const MessageSchema = new Schema(
 );
 
 MessageSchema.virtual("createdAtFormatted").get(() => {
-  return format(this.updatedAt, "dd/MM/yyyy HH:mm");
+  return format(this.createdAt, "dd/MM/yyyy HH:mm");
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
