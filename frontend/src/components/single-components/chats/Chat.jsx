e38af8f21d10dab5832fb73906b89c99cf439539
Lukas <lukasdrinkwater97@gmail.com>
2024-02-18 17:52:25 +0000

    initial commit

--- frontend/src/components/single-components/chats/Chat.jsx
+++ frontend/src/components/single-components/chats/Chat.jsx
@@ -1,19 +1,21 @@
 import SingleMessage from "./SingleMessage";
 
 export default function Chat({ chat, userId }) {
-  console.log(chat);
-  console.log(userId);
+  // console.log(chat);
+  // console.log(userId);
   return (
     <>
-      {/* map through chat messages */}
-      <div>Chat</div>
-      {chat.messages.length < 0 ? (
-        chat.messages.map((message) => {
-          <SingleMessage message={message} />;
-        })
-      ) : (
-        <p>be the first the message.</p>
-      )}
+      <div className="ChatContainer">
+        {/* map through chat messages */}
+        <div>Chat</div>
+        {chat.messages?.length > 0 ? (
+          chat.messages.map((message) => (
+            <SingleMessage key={message.id} message={message} />
+          ))
+        ) : (
+          <p>be the first the message.</p>
+        )}
+      </div>
     </>
   );
 }
