e23405f3dfdb248c7202575f3e5dc99df23afe42
Lukas <lukasdrinkwater97@gmail.com>
2024-02-18 17:52:46 +0000

    added mesage class if statement

--- frontend/src/components/single-components/chats/SingleMessage.jsx
+++ frontend/src/components/single-components/chats/SingleMessage.jsx
@@ -1,4 +1,5 @@
 export default function SingleMessage({ message, userId }) {
+  console.log(message);
   // set the message class if the message is from the user logged in or not
   let messageClass = "";
   if (message.sender.id === userId) {
