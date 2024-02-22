66f54d2bb224c9e23c9a725554a00743da671013
Lukas <lukasdrinkwater97@gmail.com>
2024-02-22 22:04:54 +0000

    added contact h1

--- frontend/src/components/single-components/chats/Chat.jsx
+++ frontend/src/components/single-components/chats/Chat.jsx
@@ -1,13 +1,14 @@
 import SingleMessage from "./SingleMessage";
 
-export default function Chat({ chatMessages, userId }) {
-  console.log(chatMessages);
-  // console.log(userId);
+export default function Chat({ chatMessages, userId, contact }) {
   return (
     <>
       <div className="ChatContainer">
         {/* map through chat messages */}
-        <div>Chat</div>
+        <div className="currentChatContact">
+          <h1>{contact}</h1>
+        </div>
+
         {chatMessages?.length > 0 ? (
           chatMessages.map((message) => (
             <SingleMessage key={message.id} message={message} userId={userId} />
