3d98f9098580af6dcfdda49304f8cde1b250fac1
Lukas <lukasdrinkwater97@gmail.com>
2024-02-22 22:02:02 +0000

    added all-users-chats route

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const chatController = require("../controllers/chatController");
 
+// Get all the users chats
+router.get("/all-users-chats", chatController.all_users_chats_get);
+
 // POST create a new chat
 router.post("/new-chat", chatController.create_new_chat_post);
 
 // GET specfic user chat
 router.get("/:chatId", chatController.user_specfic_chat_get);
-
-// Get all the users chats
-router.get("/all-users-chats", chatController.all_users_chats_get);
 
 module.exports = router;
