539630d2b8f205d7e68202d3bab683d287aa46e6
Lukas <lukasdrinkwater97@gmail.com>
2024-02-22 22:04:06 +0000

    altered code to work with the new response.data.matchedChats to take a load off the front end

--- frontend/src/components/pages/MessagesPage.jsx
+++ frontend/src/components/pages/MessagesPage.jsx
@@ -3,21 +3,37 @@ import { useNavigate } from "react-router-dom";
 import "./MessagesPage.css";
 import { useEffect, useState } from "react";
 
-export default function Messages() {
+//Import components
+import ChatPreview from "../single-components/chats/ChatPreview";
+
+export default function MessagesPage() {
   const navigate = useNavigate();
-  const [userReceiver, setUserReceiver] = useState(undefined);
-  const [currentChats, setCurrnentChats] = useState([]);
+  // const [userReceiver, setUserReceiver] = useState(undefined);
+  // const [currentChats, setCurrnentChats] = useState([]);
+  // const [allUserChats, setAllUserChats] = useState([]);
+  // const [usersContacts, setUsersContacts] = useState([]);
+  const [matchedChats, setMatchedChats] = useState([]);
 
   useEffect(() => {
+    console.log("trig");
     const getCurrentChats = async () => {
       try {
         const response = await axios({
           method: "GET",
-          url: "/chats/all-users-chats",
-          credentials: "true",
+          url: "/api/chats/all-users-chats",
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
         });
 
-        console.log(response.data);
+        console.log(response.data.matchedChats);
+
+        if (response.data.allChats?.length > 0) {
+          // setAllUserChats(response.data.allChats);
+          // setUsersContacts(response.data.usersContacts);
+          setMatchedChats(response.data.matchedChats);
+        }
       } catch (error) {
         console.log("Error getting all chats:", error);
       }
@@ -33,22 +49,29 @@ export default function Messages() {
     <>
       <div className="messagePageContainer">
         <div className="currentChats">
-          {currentChats.length > 0 ? (
+          {matchedChats.length > 0 ? (
             // map through chats to show a preview
-            <p>chat previews</p>
+            <>
+              {matchedChats.map((chat) => (
+                <ChatPreview key={chat.chatId} chat={chat} />
+              ))}
+              <p>chat previews</p>
+            </>
           ) : (
             // render new chat button
             <>
-              <p>new chat</p>
-              <div className="newChatButton">
-                <button onClick={handleNewChatClick}>Start new chat</button>
-              </div>
+              <p>
+                You dont have any chats, click Start New Chat to get chatting!
+              </p>
             </>
           )}
         </div>
         <div className="messageFeed">
           {/* map and display messages */}
           <p>messages</p>
+        </div>
+        <div className="newChatButton">
+          <button onClick={handleNewChatClick}>Start new chat</button>
         </div>
       </div>
     </>
