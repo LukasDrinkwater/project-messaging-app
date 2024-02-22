2cac82aa0abf05d3e53a4c7d58169cbcebaf4d53
Lukas <lukasdrinkwater97@gmail.com>
2024-02-22 22:05:18 +0000

    initla commit and working commit

---
+++ frontend/src/components/single-components/chats/ChatPreview.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+
+export default function ChatPreview({ chat }) {
+  const navigate = useNavigate();
+  // Find the chat that has the current user.
+  // Needs to be .some because .find expects a true
+
+  // if contact and chat arent linked into an array in the backend
+  // you can do this istead
+  // const currentContact = usersContacts.find((contact) =>
+  // chat.users.some((user) => user.id === contact.id)
+  // );
+
+  const handleGoToChatClick = () => {
+    navigate(`${chat.chatId}`);
+  };
+
+  // Cursor pointer is set in App.css
+  return (
+    <>
+      <div
+        className="chatPreiewContainer addCursorPonter"
+        onClick={handleGoToChatClick}
+      >
+        <p>{chat.contact.username}</p>
+        <p>{chat.lastMessage}</p>
+        <p>Last message: {chat.updatedAtFormatted}</p>
+      </div>
+    </>
+  );
+}
