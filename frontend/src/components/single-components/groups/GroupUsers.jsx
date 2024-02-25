7e686a20eb372a4e270f03c197b993b2137137fe
Lukas <lukasdrinkwater97@gmail.com>
2024-02-25 10:55:10 +0000

    initial working commit.

---
+++ frontend/src/components/single-components/groups/GroupUsers.jsx
@@ -0,0 +1,12 @@
+export default function GroupUsers({ groupUsers, setGroupUsers }) {
+  // console.log(groupUsers);
+  return (
+    <>
+      <div className="groupUsersContainer">
+        group users
+        {/* show the current users in the group. */}
+        {/* also have a conditional for removing them. */}
+      </div>
+    </>
+  );
+}
