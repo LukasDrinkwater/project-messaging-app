2a93d9bf6d08cd596371260b290a32ce2a0e5ad7
Lukas <lukasdrinkwater97@gmail.com>
2024-02-26 17:02:18 +0000

    added last message virtual, added lastMessage field and commented out messages field

--- backend/models/Groups.js
+++ backend/models/Groups.js
@@ -6,9 +6,14 @@ const GroupSchema = new Schema(
   {
     name: { type: String, required: true },
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    messages: [{ type: mongoose.Schema.Types.ObjectId, red: "Message" }],
+    // messages: [{ type: mongoose.Schema.Types.ObjectId, red: "Message" }],
+    lastMessage: { type: String, required: false },
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
+
+GroupSchema.virtual("lastMessageFormatted").get(function () {
+  return this.lastMessage;
+});
 
 module.exports = mongoose.model("Group", GroupSchema);
