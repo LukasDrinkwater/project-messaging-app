186cff0248135a31d18f78f619aa68e4cf152376
Lukas <lukasdrinkwater97@gmail.com>
2024-02-26 17:07:51 +0000

    added conditional render and map through groupUsers state

--- frontend/src/components/single-components/groups/GroupUsers.jsx
+++ frontend/src/components/single-components/groups/GroupUsers.jsx
@@ -1,9 +1,18 @@
 export default function GroupUsers({ groupUsers, setGroupUsers }) {
-  // console.log(groupUsers);
+  console.log(groupUsers);
   return (
     <>
       <div className="groupUsersContainer">
         group users
+        {groupUsers.length > 0 ? (
+          groupUsers.map((user) => (
+            <div className="groupContact" key={user.id}>
+              <p>{user.username}</p>
+            </div>
+          ))
+        ) : (
+          <p>Group is empty</p>
+        )}
         {/* show the current users in the group. */}
         {/* also have a conditional for removing them. */}
       </div>
