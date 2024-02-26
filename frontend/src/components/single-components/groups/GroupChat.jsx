791d46c673621b20055356e4569a6955b2f246bf
Lukas <lukasdrinkwater97@gmail.com>
2024-02-26 17:07:04 +0000

    initial commit and basic structure

---
+++ frontend/src/components/single-components/groups/GroupChat.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function GroupChat({
+  groupChatMessages,
+  userId,
+  groupChatName,
+}) {
+  return (
+    <>
+      <div className="groupChatContainer">
+        <div className="groupChatName">{/* <h1>{group.name}</h1> */}</div>
+        {/* map through the group chat messages */}
+      </div>
+    </>
+  );
+}
