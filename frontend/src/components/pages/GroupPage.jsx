b1dc9cad8d2c284bc0efe90e460f42ed46cc94b8
Lukas <lukasdrinkwater97@gmail.com>
2024-02-26 17:04:34 +0000

    added GroupPreview map and api request

--- frontend/src/components/pages/GroupPage.jsx
+++ frontend/src/components/pages/GroupPage.jsx
@@ -1,16 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import CreateGroupForm from "../single-components/forms/CreateGroupForm";
 import GroupUsers from "../single-components/groups/GroupUsers";
+import GroupPreview from "../single-components/groups/GroupPreview";
 
 export default function GroupPage() {
   // usestates to store the data
   const [addGroup, setAddGroup] = useState(false);
+  const [usersGroups, setUsersGroups] = useState([]);
   // States for GroupUsers component
   const [groupUsers, setGroupUsers] = useState([]);
 
   // useffect to call the api request
+  useEffect(() => {
+    const getUserGroups = async () => {
+      try {
+        const response = await axios({
+          method: "GET",
+          url: "/api/groups/users-groups",
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        console.log(response);
+        setUsersGroups(response.data.usersGroups);
+      } catch (error) {
+        console.log("Error getting all user groups:", error);
+      }
+    };
+
+    // Only run the useEffect if the addGroup state is false
+    if (!addGroup) {
+      getUserGroups();
+    }
+  }, []);
 
   const handleCreateNewGroupButtonClick = () => {
     setAddGroup(!addGroup);
@@ -32,11 +58,18 @@ export default function GroupPage() {
           <GroupUsers groupUsers={groupUsers} setGroupUsers={setGroupUsers} />
         </>
       ) : (
-        <div className="addGroupButton">
-          <button onClick={handleCreateNewGroupButtonClick}>
-            Create a new group
-          </button>
-        </div>
+        <>
+          <div className="groupPreviews">
+            {usersGroups.map((group) => (
+              <GroupPreview key={group.id} group={group} />
+            ))}
+          </div>
+          <div className="addGroupButton">
+            <button onClick={handleCreateNewGroupButtonClick}>
+              Create a new group
+            </button>
+          </div>
+        </>
       )}
     </>
   );
