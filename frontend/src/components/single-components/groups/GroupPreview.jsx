b346911d9191166cf49965bf3dec46059a481085
Lukas <lukasdrinkwater97@gmail.com>
2024-03-05 18:48:47 +0000

    changed Link wrapping

--- frontend/src/components/single-components/groups/GroupPreview.jsx
+++ frontend/src/components/single-components/groups/GroupPreview.jsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
-import EditGroupButton from "./EditGroupButton";
 
 export default function GroupPreview({ group }) {
   return (
     <>
-      <Link to={`${group.id}`}>
-        <div className="groupPreview">
+      <div className="groupPreview">
+        <Link to={`${group.id}`}>
           <div className="groupName">
             <p>{group.name}</p>
           </div>
           <div className="groupLastMessage">
             <p>{group.lastMessage}</p>
           </div>
-          <EditGroupButton groupId={group.id} />
-        </div>
-      </Link>
+        </Link>
+      </div>
     </>
   );
 }
