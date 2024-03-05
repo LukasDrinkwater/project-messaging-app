00abc0bad0e9df3dd3f54190987c4701ac5166e8
Lukas <lukasdrinkwater97@gmail.com>
2024-03-05 18:53:56 +0000

    added multiple routes for group editing

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -9,4 +9,30 @@ router.post("/create-group", groupController.create_new_group_post);
 // GET all of the users groups
 router.get("/users-groups", groupController.all_users_groups_get);
 
+// GET specific group
+router.get("/:groupId", groupController.get_specific_group_chat);
+
+// GET users in specific group
+router.get("/:groupId/users", groupController.get_specific_group_users);
+
+// GET specific group details
+router.get("/:groupId/details", groupController.get_specific_group_details);
+
+// PATCH update specific group name
+router.patch(
+  "/:groupId/update-name",
+  groupController.patch_update_specific_group_name
+);
+
+// DELETE specific user from the group
+router.delete(
+  "/:groupId/users/:userId/remove-user",
+  groupController.delete_specific_user
+);
+
+// PATCH specific user to group
+router.patch(
+  "/:groupId/users/:userId/add-user",
+  groupController.patch_add_specific_user_to_group
+);
 module.exports = router;
