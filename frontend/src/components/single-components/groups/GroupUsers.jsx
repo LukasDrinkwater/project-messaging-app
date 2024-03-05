b35b3e66bb992dffc0f7044aee8977af62cf0b5f
Lukas <lukasdrinkwater97@gmail.com>
2024-03-05 18:49:19 +0000

    working structure, added conditional renders

--- frontend/src/components/single-components/groups/GroupUsers.jsx
+++ frontend/src/components/single-components/groups/GroupUsers.jsx
@@ -1,20 +1,42 @@
-export default function GroupUsers({ groupUsers, setGroupUsers }) {
+export default function GroupUsers({
+  groupUsers,
+
+  createGroup = false,
+}) {
   console.log(groupUsers);
   return (
     <>
       <div className="groupUsersContainer">
         group users
-        {groupUsers.length > 0 ? (
-          groupUsers.map((user) => (
-            <div className="groupContact" key={user.id}>
-              <p>{user.username}</p>
-            </div>
-          ))
+        {!createGroup ? (
+          <>
+            {groupUsers?.length > 0 ? (
+              groupUsers.map((user) => (
+                <div className="groupContact" key={user.id}>
+                  <p>{user.username}</p>
+                </div>
+              ))
+            ) : (
+              <p>Group is empty</p>
+            )}
+          </>
         ) : (
-          <p>Group is empty</p>
+          <>
+            {groupUsers?.length > 0 ? (
+              groupUsers.map((user) => (
+                <>
+                  <link to={`/contacts/:contactId/edit`}>
+                    <div className="groupContact" key={user.id}>
+                      <p>{user.username}</p>
+                    </div>
+                  </link>
+                </>
+              ))
+            ) : (
+              <p>Group is empty</p>
+            )}
+          </>
         )}
-        {/* show the current users in the group. */}
-        {/* also have a conditional for removing them. */}
       </div>
     </>
   );
