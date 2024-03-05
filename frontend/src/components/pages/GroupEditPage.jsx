6d919d919f46c3733bae3f4d3847142d64625d7f
Lukas <lukasdrinkwater97@gmail.com>
2024-03-05 18:41:09 +0000

    added back button and EditGroupUsers EditGroupForm component calls, added aditional states

--- frontend/src/components/pages/GroupEditPage.jsx
+++ frontend/src/components/pages/GroupEditPage.jsx
@@ -1,50 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 import EditGroupUsers from "../single-components/groups/edit_group/EditGroupUsers";
 import EditGroupForm from "../single-components/forms/group_forms/EditGroupForm";
 
 export default function GroupEditPage() {
-  // const { groupId } = useParams();
-  // const [allGroupUsers, setAllGroupUsers] = useState([]);
-
-  // useEffect(() => {
-  //   const getAllGroupUsers = async () => {
-  //     try {
-  //       const response = await axios({
-  //         method: "GET",
-  //         url: `/api/groups/${groupId}/users`,
-  //         withCredentials: true,
-  //       });
-
-  //       console.log(response.data);
-  //       setAllGroupUsers(response.data.allGroupUsers);
-  //     } catch (error) {
-  //       console.log("Error getting all group users when editing group", error);
-  //     }
-  //   };
-
-  //   getAllGroupUsers();
-  // }, []);
-
-  // const handleRemoveUserClick = async (userId) => {
-  //   // api request to remove the user from the group
-
-  //   try {
-  //     const response = await axios({
-  //       method: "DELETE",
-  //       url: `/api/groups/${groupId}/users/${userId}`,
-  //       withCredentials: true,
-  //     });
-
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.log("Error removing user from group:", error);
-  //   }
-  // };
   const { groupId } = useParams();
-  const [currentGroupUsers, setAllGroupUsers] = useState([]);
+  const navigate = useNavigate();
+  // State to rerender when a user is added
+  const [groupEdited, setGroupEdited] = useState(false);
+  const [currentGroupUsers, setCurrentGroupUsers] = useState([]);
 
   useEffect(() => {
     const getAllGroupUsers = async () => {
@@ -55,35 +21,32 @@ export default function GroupEditPage() {
           withCredentials: true,
         });
 
-        // console.log(response.data);
-        setAllGroupUsers(response.data.allGroupUsers);
+        console.log(response.data);
+        setCurrentGroupUsers(response.data.allGroupUsers);
       } catch (error) {
         console.log("Error getting all group users when editing group", error);
       }
     };
 
     getAllGroupUsers();
-  }, []);
+  }, [groupEdited]);
 
   return (
     <>
       <div className="editGroupContainer">
-        <EditGroupUsers currentGroupUsers={currentGroupUsers} />
-        <EditGroupForm currentGroupUsers={currentGroupUsers} />
-        {/* <div className="editGroupUsers">
-          {allGroupUsers.map((user) => (
-            <div className="groupUser" key={user.id}>
-              <div className="name">
-                <p>{user.username}</p>
-              </div>
-              <div className="removeUserButton">
-                <button onClick={() => handleRemoveUserClick(user.id)}>
-                  Remove user
-                </button>
-              </div>
-            </div>
-          ))}
-        </div> */}
+        <div className="editGroupBackButton">
+          <button onClick={() => navigate(-1)}>Back to group chat.</button>
+        </div>
+        <EditGroupUsers
+          currentGroupUsers={currentGroupUsers}
+          groupEdited={groupEdited}
+          setGroupEdited={setGroupEdited}
+        />
+        <EditGroupForm
+          currentGroupUsers={currentGroupUsers}
+          groupEdited={groupEdited}
+          setGroupEdited={setGroupEdited}
+        />
       </div>
     </>
   );
