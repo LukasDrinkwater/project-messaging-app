5b8fb2727be1199358b5b434c5439fad288aa5ad
Lukas <lukasdrinkwater97@gmail.com>
2024-03-05 18:48:12 +0000

    added conditional render for SingleMessage, added group chat name container

--- frontend/src/components/single-components/groups/GroupChat.jsx
+++ frontend/src/components/single-components/groups/GroupChat.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import SingleMessage from "../chats/SingleMessage";
 
 export default function GroupChat({
   groupChatMessages,
@@ -8,8 +8,18 @@ export default function GroupChat({
   return (
     <>
       <div className="groupChatContainer">
-        <div className="groupChatName">{/* <h1>{group.name}</h1> */}</div>
+        <div className="groupChatName">
+          <h1>{groupChatName}</h1>
+        </div>
         {/* map through the group chat messages */}
+
+        {groupChatMessages?.length > 0 ? (
+          groupChatMessages.map((message) => (
+            <SingleMessage key={message.id} message={message} userId={userId} />
+          ))
+        ) : (
+          <p>Be the first to message</p>
+        )}
       </div>
     </>
   );
