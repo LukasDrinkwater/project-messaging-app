bbf9c874955e817a9cbead6b3fadc1f23598ad6f
Lukas <lukasdrinkwater97@gmail.com>
2024-03-05 18:41:48 +0000

    added additional states and set then in the useEffect, added NewGroupMessageForm component

--- frontend/src/components/pages/GroupChatPage.jsx
+++ frontend/src/components/pages/GroupChatPage.jsx
@@ -3,33 +3,43 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import GroupChat from "../single-components/groups/GroupChat";
+import NewGroupMessageForm from "../single-components/forms/NewGroupMessageForm";
+import EditGroupButton from "../single-components/groups/edit_group/EditGroupButton";
+import GroupUsers from "../single-components/groups/GroupUsers";
 
 export default function GroupChatPage() {
   const { groupId } = useParams();
   const [groupChatMessages, setGroupChatMessages] = useState([]);
   const [userId, setUserId] = useState("");
   const [groupChatName, setGroupChatName] = useState("");
+  const [groupChatUsers, setGroupChatUsers] = useState([]);
+  // newGroupMessagePosted triggered a rerender when the user has posted a new message
+  const [newGroupMessagePosted, setNewGroupMessagePosted] = useState(false);
 
   // useEffect, api request, get the group from the url :groupId
 
   useEffect(() => {
+    console.log("groupchatpage trigger");
     const getGroupchat = async () => {
       try {
         const response = await axios({
           method: "GET",
           url: `/api/groups/${groupId}`,
+          withCredentials: true,
         });
-
+        console.log("er");
+        console.log(response);
         setGroupChatMessages(response.data.groupChatMessages);
         setUserId(response.data.userId);
-        setGroupChatName(response.data.groupChatName);
+        setGroupChatName(response.data.groupChat.name);
+        setGroupChatUsers(response.data.groupChat.users);
       } catch (error) {
         console.log("Error getting specific group chat:", error);
       }
     };
 
     getGroupchat();
-  }, []);
+  }, [newGroupMessagePosted]);
 
   return (
     <>
@@ -43,6 +53,12 @@ export default function GroupChatPage() {
         ) : (
           <p>Loading group chat...</p>
         )}
+        <NewGroupMessageForm
+          newGroupMessagePosted={newGroupMessagePosted}
+          setNewGroupMessagePosted={setNewGroupMessagePosted}
+        />
+        <EditGroupButton />
+        <GroupUsers groupUsers={groupChatUsers} />
       </div>
     </>
   );
