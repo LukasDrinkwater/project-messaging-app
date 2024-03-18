39b90543d5221180723a3dba41ce97611ffdecec
Lukas <lukasdrinkwater97@gmail.com>
2024-03-18 21:19:09 +0000

    tidy up

--- frontend/src/components/pages/ChatPage.jsx
+++ frontend/src/components/pages/ChatPage.jsx
@@ -57,7 +57,6 @@ export default function ChatPage() {
           newMessagePosted={newMessagePosted}
           setNewMessagePosted={setNewMessagePosted}
         />
-        <div>ChatPage</div>
       </div>
     </>
   );
