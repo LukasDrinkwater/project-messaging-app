9daeef6e0866d8e9e57c8e3ab0c9ecb09040415e
Lukas <lukasdrinkwater97@gmail.com>
2024-03-18 21:28:09 +0000

    added icon imports amended structure

--- frontend/src/components/single-components/single_use/Footer.jsx
+++ frontend/src/components/single-components/single_use/Footer.jsx
@@ -3,21 +3,43 @@ import { NavLink } from "react-router-dom";
 
 import { LoginContext } from "../../../App";
 
+import groupsIcon from "../../../assets/header_and_footer/groups.png";
+import messagesIcon from "../../../assets/header_and_footer/message.png";
+import contactsIcon from "../../../assets/header_and_footer/contacts.png";
+
 export default function Footer() {
   const [loggedIn, setLoggedIn] = useContext(LoginContext);
 
   return (
     <>
       <div className="footerContainer">
-        <div className="footerMessages">
-          <NavLink to="/messages">MESSAGES</NavLink>
-        </div>
-        <div className="footerGroups">
+        <NavLink to="/messages">
+          <div className="footerMessages">
+            <div className="messagesIcon">
+              <img src={messagesIcon} alt="test" />
+            </div>
+            <p>MESSAGES</p>
+          </div>
+        </NavLink>
+        {/* <div className="footerGroups">
           <NavLink to="/groups">Groups</NavLink>
-        </div>
-        <div className="footerContacts">
-          <NavLink to="/contacts">CONTACTS</NavLink>
-        </div>
+        </div> */}
+        <NavLink to="/groups">
+          <div className="footerGroups">
+            <div className="groupsIcon">
+              <img src={groupsIcon} alt="test" />
+            </div>
+            <p>GROUPS</p>
+          </div>
+        </NavLink>
+        <NavLink to="/contacts">
+          <div className="footerContacts">
+            <div className="contactsIcon">
+              <img src={contactsIcon} alt="test" />
+            </div>
+            <p>CONTACTS</p>
+          </div>
+        </NavLink>
       </div>
     </>
   );
