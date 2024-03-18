67cc8c4976fe1115a16f8268e6002741b78fcedb
Lukas <lukasdrinkwater97@gmail.com>
2024-03-18 21:22:06 +0000

    initial late commit, added basic structure

---
+++ frontend/src/components/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+export default function LandingPage() {
+  return (
+    <>
+      <div className="landingPageContainer">
+        <p>
+          For full access please login or signup. There is the option to use a
+          sample account when signing up
+        </p>
+      </div>
+    </>
+  );
+}
