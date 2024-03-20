d73b3ac9d2ae6c3e8bc296b461f0ea00bf46fc69
Lukas <lukasdrinkwater97@gmail.com>
2024-03-20 18:48:50 +0000

    changed the Route to have element={} instead of component={}

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import { useState, createContext, useEffect } from "react";
+import { useState, createContext, useEffect, useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import axios from "axios";
+
+// import context
+import LoginContext from "./components/context/LoginContext.jsx";
 
 import "./App.css";
 
 // Import component pages
+import Layout from "./components/single-components/single_use/Layout";
 import Footer from "./components/single-components/single_use/Footer";
 import Header from "./components/single-components/single_use/Header";
 import LoginPage from "./components/pages/LoginPage";
@@ -15,12 +19,14 @@ import ContactsPage from "./components/pages/ContactsPage";
 import GroupPage from "./components/pages/GroupPage";
 import GroupChatPage from "./components/pages/GroupChatPage";
 import GroupEditPage from "./components/pages/GroupEditPage";
+import LandingPage from "./components/pages/LandingPage";
 
 // Create context
-export const LoginContext = createContext();
+// export const LoginContext = createContext();
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  // const [loggedIn, setLoggedIn] = useState(false);
+  const { setLoggedIn } = useContext(LoginContext);
 
   // runs on first page load. Initial load or after a user leaves and comes back to the
   // site and the session cookie still exists. Then updates loggedIn state
@@ -40,7 +46,7 @@ function App() {
         if (response.data.loggedIn === false) {
           setLoggedIn(false);
         } else {
-          setLoggedIn(true);
+          // setLoggedIn(true);
         }
       } catch (error) {
         console.log("Errors checking auth:", error);
@@ -52,23 +58,30 @@ function App() {
 
   return (
     <>
-      <LoginContext.Provider value={[loggedIn, setLoggedIn]}>
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" Component={MessagesPage}></Route>
-            <Route path="/login" Component={LoginPage} />
-            <Route path="/signup" Component={SignUpPage} />
-            <Route path="/messages" Component={MessagesPage} />
-            <Route path="/messages/:chatId" Component={ChatPage} />
-            <Route path="/contacts" Component={ContactsPage} />
-            <Route path="/groups" Component={GroupPage} />
-            <Route path="/groups/:groupId" Component={GroupChatPage} />
-            <Route path="/groups/:groupId/edit" Component={GroupEditPage} />
-          </Routes>
-        </div>
-        <Footer />
-      </LoginContext.Provider>
+      {/* <LoginContext.Provider value={[loggedIn, setLoggedIn]}> */}
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* Public routes */}
+            <Route path="/" element={LandingPage}></Route>
+            <Route path="/login" element={LoginPage} />
+            <Route path="/signup" element={SignUpPage} />
+
+            {/* Protected routes */}
+            <Route path="/messages" element={MessagesPage} />
+            <Route path="/messages/:chatId" element={ChatPage} />
+            <Route path="/contacts" element={ContactsPage} />
+            <Route path="/groups" element={GroupPage} />
+            <Route path="/groups/:groupId" element={GroupChatPage} />
+            <Route path="/groups/:groupId/edit" element={GroupEditPage} />
+
+            {/* Catch all */}
+          </Route>
+        </Routes>
+      </div>
+      <Footer />
+      {/* </LoginContext.Provider> */}
     </>
   );
 }
