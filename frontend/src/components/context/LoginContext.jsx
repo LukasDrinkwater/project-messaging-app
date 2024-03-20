c9c8adeba80dc3204f1b861a34433d4d594b5cc4
Lukas <lukasdrinkwater97@gmail.com>
2024-03-20 18:35:22 +0000

    added LoginContext component for easier use

---
+++ frontend/src/components/context/LoginContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+const LoginContext = createContext({});
+
+// destructure the children that are coming in.
+// Children reprisents the components that are inside the LoginProvider
+export const LoginProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <LoginContext.Provider value={{ loggedIn, setLoggedIn }}>
+      {/* children here are the components that will be nested inside the loginProvider  */}
+      {children}
+    </LoginContext.Provider>
+  );
+};
+
+export default LoginContext;
