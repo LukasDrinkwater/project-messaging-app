d199ad99493638227354e294faefcdb511abd489
Lukas <lukasdrinkwater97@gmail.com>
2024-03-21 19:23:57 +0000

    initial late commit, added Outlet and navigate components to handle protected route

---
+++ frontend/src/components/single-components/single_use/RequireAuth.jsx
@@ -0,0 +1,18 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+
+const RequireAuth = () => {
+  const { loggedIn } = useAuth();
+  const location = useLocation();
+
+  return loggedIn ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/landing-page" state={{ from: location }} replace />
+    // replace /login in the users location history with the location they came from
+    // state={{ from: location }} replace alows the user to go back to the page they were
+    // on if they press back
+  );
+};
+
+export default RequireAuth;
