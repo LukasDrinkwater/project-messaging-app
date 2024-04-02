f2510f76ce529e9583d07bf290d7241771898c26
Lukas <lukasdrinkwater97@gmail.com>
2024-04-02 21:54:24 +0100

    initial late commit, page component that calls UserProfile and ProfileImageForm components

---
+++ frontend/src/components/pages/EditProfilePage.jsx
@@ -0,0 +1,11 @@
+import UserProfile from "../single-components/contacts/UserProfile";
+import ProfileImageForm from "../single-components/forms/ProfileImageForm";
+
+export default function EditProfilePage() {
+  return (
+    <>
+      <UserProfile />
+      <ProfileImageForm />
+    </>
+  );
+}
