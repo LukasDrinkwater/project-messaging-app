75fd8699518d7759032e3dec672202b654cdb630
Lukas <lukasdrinkwater97@gmail.com>
2024-04-02 21:57:23 +0100

    initial late commit, component for basic structure of the users profile

---
+++ frontend/src/components/single-components/contacts/UserProfile.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function UserProfile() {
+  const [setUserProfile] = useState("");
+
+  useEffect(() => {
+    const getUserProfile = async () => {
+      try {
+        const response = await axios({
+          method: "GET",
+          url: "/api/contacts/profile",
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        setUserProfile(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUserProfile();
+  }, []);
+
+  return (
+    <>
+      <div className="userProfileContainer"></div>
+    </>
+  );
+}
