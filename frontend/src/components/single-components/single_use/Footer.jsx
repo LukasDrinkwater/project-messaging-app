70c77d51808b7d1e0377a0a34bdefc588e551a80
Lukas <lukasdrinkwater97@gmail.com>
2024-04-02 22:00:32 +0100

    added new footer button for going to the profile page

--- frontend/src/components/single-components/single_use/Footer.jsx
+++ frontend/src/components/single-components/single_use/Footer.jsx
@@ -1,4 +1,3 @@
-import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
 // import { LoginContext } from "../../../App";
@@ -6,6 +5,7 @@ import { NavLink } from "react-router-dom";
 import groupsIcon from "../../../assets/header_and_footer/groups.png";
 import messagesIcon from "../../../assets/header_and_footer/message.png";
 import contactsIcon from "../../../assets/header_and_footer/contacts.png";
+import profileIcon from "../../../assets/header_and_footer/profile.png";
 
 export default function Footer() {
   // const [loggedIn, setLoggedIn] = useContext(LoginContext);
@@ -16,18 +16,15 @@ export default function Footer() {
         <NavLink to="/messages">
           <div className="footerMessages">
             <div className="messagesIcon">
-              <img src={messagesIcon} alt="test" />
+              <img src={messagesIcon} alt="Icon of a message popup" />
             </div>
             <p>MESSAGES</p>
           </div>
         </NavLink>
-        {/* <div className="footerGroups">
-          <NavLink to="/groups">Groups</NavLink>
-        </div> */}
         <NavLink to="/groups">
           <div className="footerGroups">
             <div className="groupsIcon">
-              <img src={groupsIcon} alt="test" />
+              <img src={groupsIcon} alt="Icon of a group of users" />
             </div>
             <p>GROUPS</p>
           </div>
@@ -35,9 +32,17 @@ export default function Footer() {
         <NavLink to="/contacts">
           <div className="footerContacts">
             <div className="contactsIcon">
-              <img src={contactsIcon} alt="test" />
+              <img src={contactsIcon} alt="Icon of a contacts book" />
             </div>
             <p>CONTACTS</p>
+          </div>
+        </NavLink>
+        <NavLink to="/contacts/edit-profile">
+          <div className="footerProfile">
+            <div className="profileIcon">
+              <img src={profileIcon} alt="Icon of a outline of a person" />
+            </div>
+            <p>PROFILE</p>
           </div>
         </NavLink>
       </div>
