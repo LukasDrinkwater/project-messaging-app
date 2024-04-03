0c3a575b06b43162c7eadae4d27c1f89b842bb73
Lukas <lukasdrinkwater97@gmail.com>
2024-04-03 15:41:20 +0100

    added multer setup, added router for post_add_profile_image

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -3,8 +3,17 @@ const router = express.Router();
 
 const contactController = require("../controllers/contactController");
 
+// multer setup
+const multer = require("multer");
+const storage = multer;
+// const upload = multer({ dest: "upload/" }); //for saving it locally
+const upload = multer({ storage: multer.memoryStorage() });
+
 // TEST
 router.get("/add", contactController.test);
+
+// GET current user details
+router.get("/profile", contactController.get_current_user);
 
 // POST add new contact
 router.post("/add", contactController.add_new_contact_post);
@@ -19,6 +28,13 @@ router.get(
 router.delete(
   "/:contactId/remove-contact",
   contactController.delete_specific_contact
+);
+
+// POST add a new profile image
+router.post(
+  "/add-profile-image",
+  upload.single("image"), // image is the form data input name
+  contactController.post_add_profile_image
 );
 
 module.exports = router;
