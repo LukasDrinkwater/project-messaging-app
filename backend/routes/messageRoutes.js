a4bd3d2e0c43d3bd982ccb3851ef792a8cce9f45
Lukas <lukasdrinkwater97@gmail.com>
2024-04-06 20:59:26 +0100

    added middleware to routes for posting new message to allow file upload. Added multer import and config

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -3,13 +3,22 @@ const router = express.Router();
 
 const messageContoller = require("../controllers/messageController");
 
-// GET test reponse
-// router.get("/check-auth", authController.test_check);
+const multer = require("multer");
+const storage = multer;
+const upload = multer({ storage: multer.memoryStorage() });
 
 // POST new message from chat
-router.post("/new-message", messageContoller.new_message_post);
+router.post(
+  "/new-message",
+  upload.single("image"),
+  messageContoller.new_message_post
+);
 
 // POST new group message
-router.post("/new-group-message", messageContoller.new_group_message_post);
+router.post(
+  "/new-group-message",
+  upload.single("image"), // image is the form data input name
+  messageContoller.new_group_message_post
+);
 
 module.exports = router;
