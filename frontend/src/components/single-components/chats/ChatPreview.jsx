ea72761fdf0a35af91e99b0584203f79c2535561
Lukas <lukasdrinkwater97@gmail.com>
2024-04-11 16:49:20 +0100

    changed classnames to work with css module

--- frontend/src/components/single-components/chats/ChatPreview.jsx
+++ frontend/src/components/single-components/chats/ChatPreview.jsx
@@ -1,25 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import MessagesPageCss from "../../pages/MessagesPage.module.css";
 
 export default function ChatPreview({ chat }) {
   const navigate = useNavigate();
-  // Find the chat that has the current user.
-  // Needs to be .some because .find expects a true
-
-  // if contact and chat arent linked into an array in the backend
-  // you can do this istead
-  // const currentContact = usersContacts.find((contact) =>
-  // chat.users.some((user) => user.id === contact.id)
-  // );
 
   const handleGoToChatClick = () => {
     navigate(`${chat.chatId}`);
   };
 
-  // Cursor pointer is set in App.css
   return (
     <>
       <div
-        className="chatPreviewContainer addCursorPointer"
+        className={`${MessagesPageCss.chatPreviewContainer} ${MessagesPageCss.addCursorPointer}`}
         onClick={handleGoToChatClick}
       >
         <p>{chat.contact.username}</p>
