b22d59b578d9e1212e2fbadd15e575fa34df62ff
Lukas <lukasdrinkwater97@gmail.com>
2024-04-11 16:48:04 +0100

    import LandingPage.css and added a container

--- frontend/src/components/pages/LandingPage.jsx
+++ frontend/src/components/pages/LandingPage.jsx
@@ -1,11 +1,15 @@
+import "./LandingPage.css";
+
 export default function LandingPage() {
   return (
     <>
       <div className="landingPageContainer">
-        <p>
-          For full access please login or signup. There is the option to use a
-          sample account when signing up
-        </p>
+        <div className="pContainer">
+          <p>
+            For full access please login or signup. There is the option to use a
+            sample account when signing up.
+          </p>
+        </div>
       </div>
     </>
   );
